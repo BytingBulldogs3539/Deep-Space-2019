c3eae5c6ffe871f7fe98d29db5240f38bdbccbf3
Cameron (3539) <theforgelover@gmail.com>
2019-01-18 19:57:11 -0500

    Fix solenoid import

--- src/main/java/frc/robot/subsystems/Solenoids.java
+++ src/main/java/frc/robot/subsystems/Solenoids.java
@@ -1,24 +1,24 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
-import org.usfirst.frc.team3539.robot.utilities.bbDoubleSolenoid;
+import frc.robot.utilities.bbDoubleSolenoid;
 
 /**
  * Add your docs here.
  */
-public class Solenoids extends Subsystem {
+public class Solenoids extends Subsystem
+{
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
+  // Commented these out for later use, wasn't sure on names yet
 
-
-  //Commented these out for later use, wasn't sure on names yet
-
-  //	public bbDoubleSolenoid intakeSol
-  //	public bbDoubleSolenoid climbSol
+  // public bbDoubleSolenoid intakeSol
+  // public bbDoubleSolenoid climbSol
 
   @Override
-  public void initDefaultCommand() {
+  public void initDefaultCommand()
+  {
     // Set the default command for a subsystem here.
     // setDefaultCommand(new MySpecialCommand());
   }
