178c1c2c1703d542126400c3cd8165e946c31551
KennyTK <kennythakoala@gmail.com>
2019-01-18 20:06:30 -0500

    Broke ByteVision, Remove Elevator import

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -2,7 +2,6 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
-import frc.robot.motionprofiling.Constants;
 
 import com.ctre.phoenix.motorcontrol.can.*;
 import com.ctre.phoenix.motorcontrol.ControlMode;
