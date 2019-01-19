22784460f2271117f02b923a1af101fb2e67e4a9
Ashton3539 <ashtonking321@gmail.com>
2019-01-18 19:37:55 -0500

    Update solenoids subsystem, Add import, Add bbDouble Solenoid

--- src/main/java/frc/robot/subsystems/Solenoids.java
+++ src/main/java/frc/robot/subsystems/Solenoids.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import org.usfirst.frc.team3539.robot.utilities.bbDoubleSolenoid;
 
 /**
  * Add your docs here.
@@ -8,6 +9,13 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Solenoids extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+
+
+
+  //Commented these out for later use, wasn't sure on names yet
+
+  //	public bbDoubleSolenoid intakeSol
+  //	public bbDoubleSolenoid climbSol
 
   @Override
   public void initDefaultCommand() {
