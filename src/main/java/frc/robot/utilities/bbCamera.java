6d314d1f4981cd95532684dcc7524a3a762d9399
Cameron (3539) <theforgelover@gmail.com>
2019-01-24 20:36:07 -0500

    Update bbCamera.java

--- src/main/java/frc/robot/utilities/bbCamera.java
+++ src/main/java/frc/robot/utilities/bbCamera.java
@@ -4,34 +4,39 @@ import edu.wpi.cscore.*;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 
-public class bbCamera 
+public class bbCamera
 {
     private UsbCamera camera;
+
     /**
-	 * Allows us to create a camera object without causing any errors any long code in Robot.
-	 * 
-	 * @param label The name to give the camera.
-     * @param port The device number of the camera interface.
-	 */
-    public bbCamera(String label,int port)
+     * Allows us to create a camera object without causing any errors any long code
+     * in Robot.
+     * 
+     * @param label
+     *                  The name to give the camera.
+     * @param port
+     *                  The device number of the camera interface.
+     */
+    public bbCamera(String label, int port)
     {
-        //Try creating the camera however if this fails then we need to tell the driver that they won't have a camera if they don't fix it.
+        // Try creating the camera however if this fails then we need to tell the driver
+        // that they won't have a camera if they don't fix it.
         try
         {
             camera = CameraServer.getInstance().startAutomaticCapture(label, port);
             camera.setResolution(240, 240);
             camera.setFPS(20);
-            camera.setExposureManual(50);
-            camera.setBrightness(50);
-            camera.getProperty("compression").set(50);
-            camera.getProperty("default_compression").set(50);
+            camera.setExposureManual(3);
+            camera.setBrightness(10);
+            camera.getProperty("compression").set(80);
+            camera.getProperty("default_compression").set(80);
             camera.getProperty("width").set(240);
             camera.getProperty("height").set(240);
-            
+
         }
-        catch(Exception e)
+        catch (Exception e)
         {
-            DriverStation.reportError("ERROR: CAMERA "+ port +" UNPLUGGED!", e.getStackTrace());
+            DriverStation.reportError("ERROR: CAMERA " + port + " UNPLUGGED!", e.getStackTrace());
         }
     }
 }
