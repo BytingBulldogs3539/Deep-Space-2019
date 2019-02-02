56560ae8f653aff6eeaecf06ac70fc7e329a6c8c
KennyTK <kennythakoala@gmail.com>
2019-02-02 10:17:10 -0500

    Add methods to control solenoids and rollers
    Add RobotMap variable for voltageComp

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -10,11 +10,17 @@ import frc.robot.utilities.*;
  */
 public class RobotMap
 {
-    /* Robot ------------------------------ */
-    public static final int pcm = 0;
-    public static final int pdp = 0;
+    /* Robot ----------------------------------- */
+    public static final int pcm = 0; // CAN ID number
+    public static final int pdp = 0; // CAN ID number
     public static final int encTicksPerRot = 4096;
     public static boolean useLimitSwitches = true;
+
+    // Voltage "cap" to promote consistency as the voltage drops over time
+    public static final double voltageCompSaturation = 12.2;
+
+    // Amount of time to wait before reporting to Driver Station that action failed
+    public static final int timeoutMs = 20;
 
     /* DriveTrain ------------------------------ */
 
@@ -90,13 +96,10 @@ public class RobotMap
     public static final int verticateBottomMaster = 53;
     public static final int verticateBottomSlave = 52;
 
-    // Amount of time to wait before reporting to Driver Station that action failed
-    public static final int timeoutMs = 20;
-
     /* Gains / PIDs ------------------------------ */
     public static Gains elevatorGains = new Gains(.1, 0.0, 0.0, .054, 400, 1.00);
     public static Gains turretGains = new Gains(.1, 0.0, 0.0, .054, 400, 1.00);
-    public static Gains TurnPid = new Gains(.1,0.0,0.0,0.0,0.0,0.0);
+    public static Gains TurnPid = new Gains(.1, 0.0, 0.0, 0.0, 0.0, 0.0);
     public static final int manipulatorOn = 1;
     public static final int manipulatorOff = 0;
 }
--- src/main/java/frc/robot/subsystems/Manipulator.java
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -5,6 +5,9 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 import frc.robot.utilities.ByteDoubleSolenoid;
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 /**
  * Intake/Extake for Cargo comprised of two compliant wheel rollers
@@ -16,24 +19,63 @@ public class Manipulator extends Subsystem
 
   public Manipulator()
   {
+    // Initiation of Intake Talon
     master = new TalonSRX(RobotMap.manipulatorMaster);
+
+    /* Basic config for Talons */
+    TalonSRXConfiguration basicTalonConfig = new TalonSRXConfiguration();
+
+    /* Set the peak and nominal outputs */
+    basicTalonConfig.nominalOutputForward = 0.0;
+    basicTalonConfig.nominalOutputReverse = 0.0;
+    basicTalonConfig.peakOutputForward = 1.0;
+    basicTalonConfig.peakOutputReverse = -1.0;
+
+    /* Set to 27 because Talons are on 30 amp breaker */
+    basicTalonConfig.continuousCurrentLimit = 27;
+    basicTalonConfig.peakCurrentLimit = 40;
+    basicTalonConfig.peakCurrentDuration = 100;
+
+    /* Compensates for overcharging batteries. PID acts differently with different
+     * voltage. Sets Max Voltage */
+    basicTalonConfig.voltageCompSaturation = RobotMap.voltageCompSaturation;
+
+    master.configAllSettings(basicTalonConfig);
+
+    master.setNeutralMode(NeutralMode.Brake);
+
     panelIntakeSolenoid = new ByteDoubleSolenoid(RobotMap.pcm, RobotMap.manipulatorOn, RobotMap.manipulatorOff, true);
     cargoIntakeSolenoid = new ByteDoubleSolenoid(RobotMap.pcm, RobotMap.manipulatorOn, RobotMap.manipulatorOff, true);
     placementLeftSolenoid = new ByteDoubleSolenoid(RobotMap.pcm, RobotMap.manipulatorOn, RobotMap.manipulatorOff, true);
     placementRightSolenoid = new ByteDoubleSolenoid(RobotMap.pcm, RobotMap.manipulatorOn, RobotMap.manipulatorOff, true);
   }
 
+  // Grabs hatch panel from floor; not necessary for feeder station
+  public void panelIntakeSetPosition(boolean shouldActive)
+  {
+    panelIntakeSolenoid.setPosition(shouldActive);
+  }
+
+  // Actuates intake arm over bumper to grab cargo
+  public void cargoIntakeSetPosition(boolean shouldActive)
+  {
+    cargoIntakeSolenoid.setPosition(shouldActive);
+  }
+
+  // Places hatch panel onto hook tape
+  public void placementSolenoidSetPosition(boolean shouldActive)
+  {
+    placementLeftSolenoid.setPosition(shouldActive);
+    placementRightSolenoid.setPosition(shouldActive);
+  }
+
+  public void intake(double speed)
+  {
+    master.set(ControlMode.PercentOutput, speed);
+  }
+
   @Override
   public void initDefaultCommand()
   {
   }
-
-  public void triggerPanelIntake(boolean shouldActive)// True for down; False for up
-  {
-    if (shouldActive)
-      panelIntakeSolenoid.forward();
-    else
-      panelIntakeSolenoid.reverse();
-  }
-
 }
